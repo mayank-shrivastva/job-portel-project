4688ca724a977d2114718efe243ede725d38c5e4
mayank-shrivastva <100014914+mayank-shrivastva@users.noreply.github.com>
2024-03-05 17:51:45 +0530

    Add files via upload

--- js/slider.js
+++ js/slider.js
@@ -1,22 +1,41 @@
-$(document).ready(function () {
-    $('.customer-logos').slick({
-      slidesToShow: 6,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 1500,
-      arrows: false,
-      dots: false,
-      pauseOnHover: false,
-      responsive: [{
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 4 } },
-  
-      {
-        breakpoint: 520,
-        settings: {
-          slidesToShow: 3 } }] });
-  
-  
-  
+$('.owl-carousel.version-1').owlCarousel({
+    loop: true,
+    margin: 15,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 2,
+        nav: true
+      },
+      600: {
+        items: 3,
+        nav: false
+      },
+      1000: {
+        items: 4,
+        nav: true
+      }
+    }
+  });
+
+  // Slider version 2
+  $('.owl-carousel.version-2').owlCarousel({
+    loop: true,
+    margin: 15,
+    center: true,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: true
+      },
+      600: {
+        items: 2,
+        nav: false
+      },
+      1000: {
+        items: 3,
+        nav: true
+      }
+    }
   });
